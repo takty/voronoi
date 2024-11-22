ba471c860295cb06322c9c6e9803189e3417218c
Takuto Yanagida <tokutadaginaya@gmail.com>
2024-11-22 22:33:35 +0900

    Update dist and docs

--- src/_docs/index.ts
+++ src/_docs/index.ts
@@ -2,26 +2,52 @@
  * Script for Sample
  *
  * @author Takuto Yanagida
- * @version 2024-11-14
+ * @version 2024-11-22
  */
 
 import { Voronoi } from '../../voronoi';
 
 document.addEventListener('DOMContentLoaded', (): void => {
+	checkOrientationIndependence();
+	// checkRandomPartition();
+});
+
+function checkOrientationIndependence(): void {
+	const pairs: [number, number, number][][] = [
+		[[0, 3, 1], [2, 0, 2]],
+		[[0, 0, 1], [3, 2, 2]],
+		[[0, 1, 0], [2, 2, 3]],
+		[[2, 3, 0], [1, 0, 2]],
+	];
+	for (const sites of pairs) {
+		const vd: Voronoi = new Voronoi(0, 3, 0, 3, 0, 3);
+		for (const site of sites) {
+			vd.addSite(site);
+		}
+		vd.createCells();
+
+		let s: number = 0;
+		for (let i: number = 0; i < sites.length; ++i) {
+			const c: number = vd.countGrids(i, 0.1);
+			console.log(sites[i].toString(), c);
+			s += c;
+		}
+		console.log(s);
+	}
+}
+
+function checkRandomPartition(): void {
 	const sites: [number, number, number][] = [
-		[0, 6, 5], [0, 4, 4],
-		// [6, 0, 5], [4, 0, 4],
-		// [6, 5, 0], [4, 4, 0],
-		// [Math.round(Math.random() * 10), 0, Math.round(Math.random() * 10)],
-		// [Math.round(Math.random() * 10), 0, Math.round(Math.random() * 10)],
+		[Math.round(Math.random() * 3), Math.round(Math.random() * 3), 0],
+		[Math.round(Math.random() * 3), Math.round(Math.random() * 3), 0],
 	]
-	const vd: Voronoi = new Voronoi(0, 10, 0, 10, 0, 10);
+	const vd: Voronoi = new Voronoi(0, 3, 0, 3, 0, 3);
 	for (const site of sites) {
 		vd.addSite(site);
 	}
 	vd.createCells();
 
 	for (let i: number = 0; i < sites.length; ++i) {
-		console.log(sites[i].toString(), vd.countGrids(i, 1));
+		console.log(sites[i].toString(), vd.countGrids(i, 0.1));
 	}
-});
+}
